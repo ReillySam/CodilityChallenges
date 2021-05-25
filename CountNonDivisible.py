8a4f5fd0bbcb818ea547e28442e49bee87610755
Sam <samreilly132@icloud.com>
2021-05-25 16:29:07 +0200

    Prime & Composite Numbers Sieve of Eratosthenes

---
+++ CountNonDivisible.py
@@ -0,0 +1,64 @@
+'''
+You are given an array A consisting of N integers.
+
+For each number A[i] such that 0 ≤ i < N, we want to count the number of elements of the array that are not the divisors of A[i]. We say that these elements are non-divisors.
+
+For example, consider integer N = 5 and array A such that:
+
+    A[0] = 3
+    A[1] = 1
+    A[2] = 2
+    A[3] = 3
+    A[4] = 6
+For the following elements:
+
+A[0] = 3, the non-divisors are: 2, 6,
+A[1] = 1, the non-divisors are: 3, 2, 3, 6,
+A[2] = 2, the non-divisors are: 3, 3, 6,
+A[3] = 3, the non-divisors are: 2, 6,
+A[4] = 6, there aren't any non-divisors.
+Write a function:
+
+class Solution { public int[] solution(int[] A); }
+
+that, given an array A consisting of N integers, returns a sequence of integers representing the amount of non-divisors.
+
+Result array should be returned as an array of integers.
+
+For example, given:
+
+    A[0] = 3
+    A[1] = 1
+    A[2] = 2
+    A[3] = 3
+    A[4] = 6
+the function should return [2, 4, 3, 2, 0], as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..50,000];
+each element of array A is an integer within the range [1..2 * N].
+'''
+
+def Soluiton(A):
+    length = len(A)
+    count = [0] * length
+
+    if length == 0 : return 0
+
+    # Solution 1: 55%
+    for i in range(length):
+        for j in range(length):
+            if i != j:
+                if A[i] % A[j] != 0:
+                    count[i] += 1
+
+    # Solution 2
+    # set_A = set(A)
+    #
+    # for i in A:
+    #     if i % 2 == 0 and  i in set_A:
+
+    return count
+
+print(Soluiton( [3, 1, 2, 3, 6] ))
